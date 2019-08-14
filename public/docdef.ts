7201c3f3069d4f916067489cfcc035888bd3fadc
Zachary Simon <zachary1006@hotmail.com>
2019-08-14 15:27:29 -0700

    add typescript to project and re-write docdef.js as docdef.ts

--- public/docdef.ts
+++ public/docdef.ts
@@ -1,36 +1,39 @@
-import _ from 'lodash';
+// @ts-ignore
 import { fieldFormats } from 'ui/registry/field_formats';
+import $ from 'jquery';
 
-/** Shamelessly stolen from: 
+// declare const $: any
+
+/** Shamelessly stolen from:
  * https://stackoverflow.com/a/901144
  */
-export function getParameterByName(name, url) {
+export function getParameterByName(name: string, url?: string) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
+    const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+    const results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, ' '));
 }
 
-export function DocDefNameProvider(FieldFormat) {
+export function DocDefNameProvider(FieldFormat: any) {
 
     const docdefIdMap = new Map();
-    let ngParams = getParameterByName('ng-url');
+    const ngParams = getParameterByName('ng-url');
     $.ajax({
         type: 'GET',
         url: ngParams + '/api/definitions',
-        accept: 'application/json',
+        // accepts: 'application/json',
         xhrFields: {
             withCredentials: true
         },
-        success: function(result) {
-            for (let definition of result) {
-                docdefIdMap.set(definition.id, definition.hasOwnProperty('name') ? definition.name : 'New Document Definition (' + definition.id + ')');
+        success: function (result: any[]) {
+            for (const definition of result) {
+                docdefIdMap.set(definition.id, definition.name || 'New Document Definition (' + definition.id + ')');
             }
-        }, 
-        error: function(error) {
+        },
+        error: function (error) {
             console.log(error);
         }
     });
@@ -38,19 +41,19 @@ export function DocDefNameProvider(FieldFormat) {
     return class DocDefName extends FieldFormat {
         static id = 's4i-docdefname';
         static title = 'Document Definition Name';
-        static fieldType = [ 
+        static fieldType = [
             'string',
             'number'
         ];
 
-        _convert(value) {
-            let intValue = parseInt(value);
+        _convert(value: string) {
+            const intValue = parseInt(value);
             if (docdefIdMap.has(intValue)) {
                 return docdefIdMap.get(intValue);
             } else {
-                return 'New Document Definition (' + value + ')'
+                return 'New Document Definition (' + value + ')';
             }
         }
-    }
+    };
 }
-fieldFormats.register(DocDefNameProvider)
+fieldFormats.register(DocDefNameProvider);
